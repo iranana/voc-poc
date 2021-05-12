5fd15892773d80325e87886d3128ba0b3776f9fa
Ira Nana <ira.nana@aotal.com>
2021-05-13 10:24:33 +1200

    Add rudimentary reconnect functionality

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const usb = require('usb')
 const yargs = require('yargs')
 const fs = require('fs')
 
-
 const argv = yargs
     .option('f', {
         alias: 'file',
@@ -28,72 +27,79 @@ const argv = yargs
     })
     .option('v', {
         alias: 'verbose',
-        describe: 'be noisy - doesn not play well with -o',
+        describe: 'be noisy - does not play well with -o',
         type: 'boolean'
     })
     .help()
     .alias('help', 'h')
     .argv;
 
-var goggles = usb.findByIds("0x2ca3", "0x1f")
+const stream = () => {
+    const goggles = usb.findByIds("0x2ca3", "0x1f");
 
-if(!goggles) {
-    console.error("Goggles USB device not found. Please connect your goggles and restart the script.")
-    process.exit(1)
-}
-goggles.open()
-if(!goggles.interfaces) {
-    console.error("Couldn't open Goggles USB device")
-    process.exit(1)
-}
-var interface = goggles.interface(3)
-interface.claim()
-if(!interface.endpoints) {
-    console.error("Couldn't claim bulk interface")
-    process.exit(1)
-}
-
-if(!argv.f && !argv.o) {
-    console.log("warning: no outputs specified")
-    argv.v = true
-}
-
-var fd
-
-var inpoint = interface.endpoints[1]
-inpoint.timeout = 100
-//outpoint.timeout = 200
-
-var outpoint = interface.endpoints[0]
-var magic = Buffer.from("524d5654", "hex")
-
-outpoint.transfer(magic, function(error) {
-    if(error) {
-        console.error(error)
+    if (!goggles) {
+        console.error("Goggles USB device not found. Please connect your Goggles.")
+        return usb.on("attach", stream);
     }
-    console.debug("send magic bytes")
+    
+    goggles.open()
+    
+    if (!goggles.interfaces) {
+        console.error("Couldn't open Goggles USB device")
+        process.exit(1)
+    }
+    
+    const interface = goggles.interface(3)
+    interface.claim()
+    
+    if (!interface.endpoints) {
+        console.error("Couldn't claim bulk interface")
+        process.exit(1)
+    }
+    
+    if (!argv.f && !argv.o) {
+        console.log("Warning: no outputs specified")
+        argv.v = true
+    }
+    
+    let fd
+    const inpoint = interface.endpoints[1]
+    const outpoint = interface.endpoints[0]
+    const magic = Buffer.from("524d5654", "hex")
 
-})
-inpoint.addListener("data", function(data) {
-    if(argv.o) {
-        process.stdout.write(data)
-    }
-    if(argv.v) {
-        console.debug("received "+data.length+" bytes")
-    }
-    if(argv.f) {
-        if(!fd) {
-            fd = fs.openSync(argv.f, "w")
-            if(!fd) {
-                console.error("couldn't open file "+ argv.f + ": "+err)
-                process.exit(1)
-            }
+    inpoint.timeout = 100
+    
+    outpoint.transfer(magic, error => {
+        if (error) {
+            console.error(error)
         }
-        fs.writeSync(fd, data)
-    }
-})
-inpoint.addListener("error", function(error) {
-    console.error(error)
-})
-inpoint.startPoll(argv.q, argv.s)
+        console.debug("Send magic bytes")
+    })
+    
+    inpoint.addListener("data", data => {
+        if (argv.o) {
+            process.stdout.write(data)
+        }
+        if (argv.v) {
+            console.debug(`Received ${data.length} bytes`)
+        }
+        if (argv.f) {
+            if (!fd) {
+                fd = fs.openSync(argv.f, "w")
+                if (!fd) {
+                    console.error(`Couldn't open file ${argv.f}: ${err}`)
+                    process.exit(1)
+                }
+            }
+            fs.writeSync(fd, data)
+        }
+    })
+    
+    inpoint.addListener("error", error => {
+        console.error(error)
+    })
+    
+    inpoint.startPoll(argv.q, argv.s)    
+}
 
+stream();
